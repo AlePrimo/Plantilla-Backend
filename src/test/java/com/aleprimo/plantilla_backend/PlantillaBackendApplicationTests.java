06a900951cb1c8d1ad6f41229cbeaa16c02f2a4a
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-06-23 20:08:27 -0300

    Primer commit : subiendo el proyecto

---
+++ src/test/java/com/aleprimo/plantilla_backend/PlantillaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleprimo.plantilla_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantillaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
