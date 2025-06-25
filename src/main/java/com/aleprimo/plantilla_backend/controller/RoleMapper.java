2c60ff4205e94b5e6a5d92e277f438df74aeecdd
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-06-25 20:43:04 -0300

    Creacion de mapper para role

--- src/main/java/com/aleprimo/plantilla_backend/controller/RoleMapper.java
+++ src/main/java/com/aleprimo/plantilla_backend/controller/RoleMapper.java
@@ -1,7 +1,37 @@
 package com.aleprimo.plantilla_backend.controller;
 
+import com.aleprimo.plantilla_backend.dto.RoleDTO;
+import com.aleprimo.plantilla_backend.model.Role;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RoleMapper {
+
+    public RoleDTO toDto(Role role){
+        if (role == null) {
+            return null;
+        }
+
+        return RoleDTO.builder()
+                .id(role.getId())
+                .name(role.getName())
+                .build();
+    }
+
+
+
+    public Role toEntity(RoleDTO roleDTO){
+        if (roleDTO == null) {
+            return null;
+        }
+
+        return Role.builder()
+                .id(roleDTO.getId())
+                .name(roleDTO.getName())
+                .build();
+    }
+
+
+
+
 }
