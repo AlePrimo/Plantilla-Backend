75f79e07d305e396b039a2f450a1cae5c13e1220
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-07-01 19:47:04 -0300

    Agregado de base de datos H2 para desarrollo y clase de configuracion para Test

--- src/test/java/com/aleprimo/plantilla_backend/controller/UserControllerTest.java
+++ src/test/java/com/aleprimo/plantilla_backend/controller/UserControllerTest.java
@@ -1,22 +1,19 @@
 package com.aleprimo.plantilla_backend.controller;
 
 import com.aleprimo.plantilla_backend.dto.UserDTO;
-
 import com.aleprimo.plantilla_backend.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
-
-import java.util.List;
-
+import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class UserControllerTest {
 
     @LocalServerPort
